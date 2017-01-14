0d0a5218edbbf3e450cd1c431141447574764ba2
RajaPrahlad <npreddy3339@gmail.com>
2017-01-14 12:34:57 -0500

    Initial Commit

---
+++ Sending Email/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Sending Email
+//
+//  Created by Prahlad on 1/14/17.
+//  Copyright © 2017 Prahlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Sending Email/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sending Email
+//
+//  Created by Prahlad on 1/14/17.
+//  Copyright © 2017 Prahlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
